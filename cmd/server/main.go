89cca3c07ef932dc41e734aa0bf5042341ef1a8f
Evan Pierce <evanpierce@aim.com>
2021-02-10 01:50:16 -0500

    more setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"strconv"
+	"time"
 
 	"github.com/TensorBeat/Datalake/internal/controller"
 	"github.com/TensorBeat/Datalake/pkg/proto"
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 func main() {
 
-	portPtr := flag.Int("port", 3912, "port to run grpc service")
-	flag.Parse()
-	listenAddress := ":" + strconv.Itoa(*portPtr)
-
+	//Setup Logging
 	logger, err := zap.NewProduction()
 
 	if err != nil {
@@ -28,6 +30,30 @@ func main() {
 
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
+
+	//Dotenv
+	err = godotenv.Load("../../.env") // .env in base directory
+	if err != nil {
+		sugar.Warnf("No .env loaded: %v", err)
+	}
+
+	listenAddress := ":" + os.Getenv("DL_PORT")
+	MongoURI := os.Getenv("MONGO_URI")
+
+	ctx := context.Background()
+
+	mongoCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	mongoClient, err := mongo.Connect(mongoCtx, options.Client().ApplyURI(MongoURI))
+	if err != nil {
+		sugar.Fatalf("Couldn't connect to mongo: %v", err)
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	err = mongoClient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		sugar.Fatalf("Couldn't ping mongo: %v", err)
+	}
 
 	listener, err := net.Listen("tcp", listenAddress)
 	if err != nil {
